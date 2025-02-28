fdea7ae7f62e92fe9652e8345f7b90f8852958b0
seb1o <eusebio.veizi@gmail.com>
2025-02-28 01:02:40 +0100

    99

---
+++ script.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById('my-canvas');
+
+const ctx = canvas.getContext('2d');
+
+
+function drawInvertedTriangle(x, y, size, color) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - size / 2, y + size);
+    ctx.lineTo(x + size / 2, y + size);
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
+  }
+  
+  let yPosition = canvas.height;
+  
+  
+  setInterval(() => {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    const colors = ['white', 'gray', 'beige'];
+    const sizes = [200, 150, 100];
+    const baseYPosition = canvas.height / 2 - 100; 
+  
+    for (let i = 0; i < 3; i++) {
+      drawInvertedTriangle(canvas.width / 2, baseYPosition + (i * 100), sizes[i], colors[i]);
+    }
+  
+  }, 500);
+
